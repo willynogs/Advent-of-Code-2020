4a33ebbbc0cf43aba0c8fee6b03d4ea2e61bb510
Will Naugle <willynogs@gmail.com>
2020-12-01 22:51:05 -0500

    modify day 1 for part 2

--- day1/day1.go
+++ day1/day1.go
@@ -10,25 +10,31 @@ import (
 
 func main() {
 	content, err := ioutil.ReadFile("input.txt")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	entries := strings.Split(string(content), "\n")
 
-	for x := 0; x < (len(entries) - 1); x++ {
-		for y := (x + 1); y < len(entries[x:]); y++ {
-			xI, xErr := strconv.Atoi(entries[x])
-			if xErr != nil {
-				log.Fatal(xErr)
-			}
-			yI, yErr := strconv.Atoi(entries[y])
-			if yErr != nil {
-				log.Fatal(yErr)
-			}
-			if xI+yI == 2020 {
-				fmt.Printf("Answer: %d", xI*yI)
+	for x := 0; x < (len(entries) - 2); x++ {
+		for y := (x + 1); y < (len(entries) - 1); y++ {
+			for z := (y + 1); z < len(entries); z++ {
+				xI, xErr := strconv.Atoi(entries[x])
+				if xErr != nil {
+					log.Fatal(xErr)
+				}
+				yI, yErr := strconv.Atoi(entries[y])
+				if yErr != nil {
+					log.Fatal(yErr)
+				}
+				zI, zErr := strconv.Atoi(entries[z])
+				if zErr != nil {
+					log.Fatal(zErr)
+				}
+				if xI+yI+zI == 2020 {
+					fmt.Printf("Answer: %d", xI*yI*zI)
+					fmt.Println()
+				}
 			}
 		}
 	}
